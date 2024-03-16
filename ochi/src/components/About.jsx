4a78ae65ccd989f2f99ef16d8b925f59455b4077
bbiijjeett <bijeetnathbtech@gmail.com>
2024-03-16 23:36:07 +0530

    img updated in ochi project

--- ochi/src/components/About.jsx
+++ ochi/src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <div className="w-2 h-2 rounded-full bg-zinc-100"></div>
           </button>
         </div>
-        <div className="w-1/2 h-[60vh] rounded-2xl bg-[#b5d442]"></div>
+        <div className="w-1/2 h-[60vh] rounded-2xl bg-cover bg-[url('https://images.unsplash.com/photo-1682687221080-5cb261c645cb?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"></div>
       </div>
     </div>
   );
